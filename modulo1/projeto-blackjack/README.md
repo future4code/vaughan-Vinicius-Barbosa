0eb3b6a7de106d6062f1074354591eb96bc70aa7
Vinicius <viniciuscicone@gmail.com>
2021-12-26 17:09:18 -0300

    Updade no projeto

---
+++ modulo1/projeto-blackjack/README.md
@@ -0,0 +1,5 @@
+<h1>Projeto black jack</h1>
+
+```
+Projeto e um jogo de cartas onde o objetivo é derrotar o dealer(Computador). Para fazer isso, você deve ter uma mão em que a pontuação seja mais elevada do que a mão do dealer(Computador), mas não exceda 21 pontos no valor total. Como alternativa, você pode ganhar tendo uma pontuação menor que 22 quando o valor da mão do dealer ultrapassar 21.
+```
--- modulo1/projeto-blackjack/nossoJogo.js
+++ modulo1/projeto-blackjack/nossoJogo.js
@@ -15,49 +15,47 @@ console.log("Bem-vindo(a) ao jogo de blackjack")
 
 
 
-alert(`Abra o prompt`)
 
-let jogo = confirm("Quer iniciar uma rodada?")
+
 
 function jogarBlack() {
 
+    let jogo = confirm("Inicio")
+    var cartaUsuarioA = comprarCarta()
+    var cartaUsuarioB = comprarCarta()
+    var cartaPcA = comprarCarta()
+    var cartaPcB = comprarCarta()
 
-   var cartaUsuarioA = comprarCarta()
-   var cartaUsuarioB = comprarCarta()
-   var cartaPcA = comprarCarta()
-   var cartaPcB = comprarCarta()
+    if (jogo === true) {
 
-   if (jogo === true) {
-
-      let pontuacaoUser = cartaUsuarioA.valor + cartaUsuarioB.valor
-      let pontuacaoPc = cartaPcA.valor + cartaPcB.valor
-      alert(`O jogo começou`)
-      alert(`Cartas do usuario`)
-      console.log(`A pontuaçao do usuario foi ${pontuacaoUser}, cartas ${cartaUsuarioA.texto} ${cartaUsuarioB.texto}`)
-      alert(`Cartas do computador`)
-      console.log(`A pontuaçao do computador foi ${pontuacaoPc}, cartas ${cartaPcA.texto} ${cartaPcB.texto}`)
-
-
-      if (pontuacaoUser > pontuacaoPc) {
-         alert(`Parabens voce ganhou!`)
-      } else if (pontuacaoUser < pontuacaoPc) {
-         alert("Voce perdeu :( tente novamente")
-      } else if (pontuacaoUser === pontuacaoPc) {
-         alert("Jogo empatou -_-")
-      }
-      let jogarNovamente = confirm("Jogar novamente ?")
-      if (jogarNovamente === true) {
-         jogarBlack()
-      } else {
-         alert("Ate mais !")
-      }
+        let pontuacaoUser = cartaUsuarioA.valor + cartaUsuarioB.valor
+        let pontuacaoPc = cartaPcA.valor + cartaPcB.valor
+        alert(`O jogo começou`)
+        alert(`Cartas do usuario`)
+        console.log(`A pontuaçao do usuario foi ${pontuacaoUser}, cartas ${cartaUsuarioA.texto} ${cartaUsuarioB.texto}`)
+        alert(`Cartas do computador`)
+        console.log(`A pontuaçao do computador foi ${pontuacaoPc}, cartas ${cartaPcA.texto} ${cartaPcB.texto}`)
 
 
-   } else {
+        if (pontuacaoUser > pontuacaoPc) {
+            alert(`Parabens voce ganhou!`)
+        } else if (pontuacaoUser < pontuacaoPc) {
+            alert("Voce perdeu :( tente novamente")
+        } else if (pontuacaoUser === pontuacaoPc) {
+            alert("Jogo empatou -_-")
+        }
+        let jogarNovamente = confirm("Jogar novamente ?")
+        if (jogarNovamente === true) {
+            jogarBlack()
+        } else {
+            alert("Ate mais !")
+        }
 
-      alert(`O jogo acabou`)
-   }
+
+    } else {
+
+        alert(`O jogo acabou`)
+    }
 
 }
 
-jogarBlack() 
