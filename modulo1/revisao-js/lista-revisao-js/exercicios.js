98b10476dde46ef96e58de8f2f265c5848272bbe
ViniciusBarbosa <92874088+viniciuscicone@users.noreply.github.com>
2021-12-27 17:16:48 -0300

    Exercicio 14

--- modulo1/revisao-js/lista-revisao-js/exercicios.js
+++ modulo1/revisao-js/lista-revisao-js/exercicios.js
@@ -134,8 +134,15 @@ function retornaPessoasNaoAutorizadas(pessoas) {
 
 // EXERCÍCIO 14
 function retornaContasComSaldoAtualizado(contas) {
-
+        for (divida of contas) {
+        for (compra of divida.compras) {
+            divida.saldoTotal -= compra;
+            divida.compras = [];
+        }
+    }
+    return contas;
 }
+
 
 // EXERCÍCIO 15A
 function retornaArrayOrdenadoAlfabeticamente(consultas) {
