ed48cee99c5fc94795829c0957c5036968ea677d
ViniciusBarbosa <92874088+viniciuscicone@users.noreply.github.com>
2022-01-16 20:51:01 -0300

    Finalizado

--- modulo2/estados-insta4/insta4/src/App.js
+++ modulo2/estados-insta4/insta4/src/App.js
@@ -27,22 +27,59 @@ class App extends React.Component {
         nomeUsuario: "vini",
         fotoUsuario: "https://picsum.photos/50/50?a=2",
         fotoPost: "https://picsum.photos/200/150?a=2"
-      },
-      {
-        nomeUsuario: "labenu",
-        fotoUsuario: "https://picsum.photos/50/50?a=3",
-        fotoPost: "https://picsum.photos/200/150?a=3"
       }
-    ] 
+     
+    ],
+
+    valorInputNome: "",
+    valorInputFoto: "",
+    valorInputFotoPost: ""
   };
-  
-  
 
-
-
-  
-  render() {
+  adicionaPost = () => {
     
+    const novoPost = {
+      
+      nomeUsuario: this.state.valorInputNome,
+      fotoUsuario: 'https://picsum.photos/50/50?a=' + this.state.valorInputFoto,
+      fotoPost: 'https://picsum.photos/200/150?a=' + this.state.valorInputFotoPost
+    };
+   
+   const novoPosts = [...this.state.post, novoPost];
+
+   this.setState({ post: novoPosts });
+ 
+ };
+
+
+
+ onChangeInputPost = (event) => {
+ 
+  this.setState({ valorInputNome: event.target.value });
+};
+
+
+
+
+onChangeInputFoto = (event) => {
+
+  this.setState({ valorInputFoto: event.target.value });
+};
+
+
+
+
+onChangeInputFotoGrande = (event) => {
+
+  this.setState({ valorInputFotoPost: event.target.value });
+};
+
+  
+
+
+
+  render() {
+  
   const feed = this.state.post.map((pessoa) => {
     
     return (
@@ -59,9 +96,28 @@ class App extends React.Component {
       
       <MainContainer>
         {feed}
+        <input
+            value={this.state.valorInputNome}
+            onChange={this.onChangeInputPost}
+            placeholder={"Seu nome"}
+          />
+          <input
+            value={this.state.valorInputFoto}
+            onChange= {this.onChangeInputFoto}
+            placeholder={"Foto de 1 a 10"}
+          />
+          <input
+            value={this.state.valorInputFotoPost}
+            onChange={this.onChangeInputFotoGrande}
+            placeholder={"Foto de 1 a 10"}
+          />
+
+          <button onClick={this.adicionaPost} >Adicionar post</button>
+
       </MainContainer>
-    );
+    );  
   }
+    
 }
 
 export default App;
