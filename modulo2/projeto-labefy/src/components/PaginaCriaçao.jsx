01ff1b84ad5af31819f55296f46f2541bfb80863
ViniciusBarbosa <92874088+viniciuscicone@users.noreply.github.com>
2022-01-27 16:59:08 -0300

    primeiro commit

---
+++ modulo2/projeto-labefy/src/components/PaginaCriaçao.jsx
@@ -0,0 +1,18 @@
+
+
+
+import axios from "axios"
+
+
+
+export function PaginaCriaçao(props) {
+
+
+    return(
+
+        <div>ESSA E MINHA PAGINA DE CRIAÇAO !</div>
+
+    )
+
+
+} 
---
+++ modulo2/projeto-labefy/src/components/PaginaLista.jsx
@@ -0,0 +1,15 @@
+
+
+
+
+export function PaginaLista(props) {
+
+    return(
+        <>
+           <div>{ props.lista }</div>
+           <button onClick={ props.paginaDoVideo } >IR PARA MUSICAS</button>
+            
+        </>
+     
+    )
+} 
