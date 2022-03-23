b41005f03f68d0cf7245415c0b6413293a452b37
ViniciusBarbosa <92874088+viniciuscicone@users.noreply.github.com>
2022-03-23 16:47:37 -0300

    exercicio type-script

---
+++ modulo4/intro-typescript/src/exercicio2.ts
@@ -0,0 +1,8 @@
+function imprimeTresCoresFavoritas() {
+   const cor1:string = 'azul'
+   const cor2:string = 'vermelho'
+   const cor3:string = 'verde'
+   console.log([cor1, cor2, cor3])
+}
+
+imprimeTresCoresFavoritas()
