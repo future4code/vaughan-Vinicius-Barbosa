60b25fd8d12af0e9c7ac9667a3b9262fd851c3d5
ViniciusBarbosa <92874088+viniciuscicone@users.noreply.github.com>
2022-03-24 16:55:53 -0300

    commit pratica type script

---
+++ modulo4/aprofundamento-typescript/src/exercicios/exercicio5.ts
@@ -0,0 +1,16 @@
+console.log('exercicio 5')
+//a) Compare esse arquivo com o que criamos durante a aula 
+//(ele está disponível na área de configuração do projeto ali em cima). Leia as 
+//descrições sobre cada uma das propriedades. Alguma configuração que chamou sua atenção? 
+//O que mudou em comparação com o arquivo criado pelos slides?
+
+
+/* 
+RESPOSTA=  
+Este comando e utilizado para criar um arquivo padrao onde e possivel alterar a 
+forma com que o projeto e executado na maquina, o arquivo que utilizamos na aula 
+ja esta pre configurado com alguns parametros uteis para nos auxiliar no desenvolvimento
+do aplicativo.
+O arquivo tsconfig.json que utilizados por exemplo foi alterado a saida dos arquivos 
+que sao transpilados em javascript ja o arquivo padrao vem com essa configuraçao desativada.
+*/
