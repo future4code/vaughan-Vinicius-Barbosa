cf9874638feaaf8617a3be9c78d7b71d33c8e539
ViniciusBarbosa <92874088+viniciuscicone@users.noreply.github.com>
2022-03-26 13:15:23 -0300

    update

--- modulo4/projeto-listaTypescript/src/Exercício8.ts
+++ modulo4/projeto-listaTypescript/src/Exercício8.ts
@@ -1,1 +1,28 @@
 console.log('Exercicio 8')
+
+
+function checaRenovacaoRG_BR(anoNascimento: string, anoEmissao: string) {
+
+   const anoNas = anoNascimento.split('/')
+   const anoEmi = anoEmissao.split('/')
+   const anoAtual: number = new Date().getFullYear()
+
+   let idade = anoAtual - parseInt(anoNas[2])
+   let tempoCarteira = anoAtual - parseInt(anoEmi[2])
+
+   if (idade <= 20) {
+      return tempoCarteira >= 5 ? true : false
+
+   } else if (idade >= 20 || idade <= 50) {
+      return tempoCarteira >= 10 ? true : false
+
+   } else if (idade > 50) {
+      return tempoCarteira >= 15 ? true : false
+
+   } else {
+      return false
+   }
+}
+
+console.log(checaRenovacaoRG_BR("27/07/1995", "18/03/2012"))
+
