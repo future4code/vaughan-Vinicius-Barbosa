fb65a7e988850abfdf870cf11e86671a6740ffb7
ViniciusBarbosa <92874088+viniciuscicone@users.noreply.github.com>
2022-03-28 16:39:24 -0300

    criar end-points

---
+++ modulo4/intro-express/src/data.ts
@@ -0,0 +1,73 @@
+export const data = {
+   
+   users: [
+      {
+         id: 1,
+         name: 'joaozinho',
+         phone: '11-3733-3333',
+         email: 'jbola@hmail.com',
+         site: 'www.jb.com',
+         posts: [
+            {
+               id: 1,
+               title: 'frase de impacto',
+               body: 'frase de impacto que fara voce pensar',
+               userId: 1
+            },
+            {
+               id: 2,
+               title: 'frase matinal',
+               body: 'frase que ira fazer voce se sentir animado para começar o dia bem',
+               userId: 1
+            }
+         ]
+      },
+      {
+         id: 2,
+         name: 'mariazinha',
+         phone: '11-3533-3333',
+         email: 'opa@hmail.com',
+         site: 'www.pais.com',
+         posts: [
+            {
+               id: 1,
+               title: 'rasef amatinl',
+               body: 'frasquse rasque ira fazer voce sezer sentir animntiado para caomeçar o dia bem',
+               userId: 2
+            }
+         ]
+      },
+      {
+         id: 3,
+         name: 'ronaldo',
+         phone: '11-3533-3333',
+         email: 'bomba@hmail.com',
+         site: 'www.volta.com',
+         posts: [
+            {
+               id: 1,
+               title: 'usuario3',
+               body: 'usuario3 comentario',
+               userId: 3
+            }
+         ]
+      },
+      {
+         id: 4,
+         name: 'marilene',
+         phone: '11-3533-3333',
+         email: 'marli@maildopovo.com',
+         site: 'www.marli.com',
+         posts: [
+            {
+               id: 1,
+               title: 'usuario3',
+               body: 'usuario3 comentario',
+               userId: 4
+            }
+         ]
+      }
+   ]
+
+}
+
---
+++ modulo4/intro-express/src/index.ts
@@ -0,0 +1,108 @@
+import express from 'express';
+import cors from 'cors';
+import { data } from './data';
+
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+
+// ENDPOINTS
+
+// 1
+app.get("/", (req, res) => {
+ 
+   console.log('funcionou console')
+
+   res.status(200).send('testando');
+});
+
+
+
+
+
+// getAllPlaylists
+app.get("/posts", (req, res) => {
+   
+   const playlists = data.users.map((user) => {
+       return user.posts
+   });
+
+   const resultPlaylists = playlists.flat(1) // flat(profundidade da busca)
+   // [1,[2,3], 4] -> flat(1) -> [1,2,3,4] 
+
+
+   
+
+
+   res.status(200).send(resultPlaylists);
+});
+
+
+
+
+/* 
+// getPlaylistTracks
+app.get("/tracks", (req, res) => {
+
+   const playlists = data.map((user) => {
+       return user;
+   }).flat(1);
+
+   const tracks = playlists.map((playlist) => {
+       return playlist
+   }).flat(1);
+
+   res.status(200).send(tracks);
+})
+ */
+
+
+
+
+
+// deletePlaylistsByUserId
+
+app.delete("/playlists/:userId", (req, res) => {
+   const userId = req.params.userId;
+
+ /*   const usersUpdated = users.map((user) => {
+       if (user.id === userId) {
+           return { ...user, playlists: [] }
+       } else {
+           return user
+       }
+   }); */
+
+   res.send(userId);
+})
+
+
+
+
+
+/* 
+// deleteTracksByPlaylistId
+app.delete("/tracks/:playlistId", (req, res) => {
+   const playlistId = req.params.playlistId;
+
+   const usersUpdated = data.map((user) => {
+       //????????????codigo aqui
+   })
+
+   res.send(usersUpdated);
+}) */
+
+
+
+
+
+
+
+app.listen(3003, () => {
+   console.log("Backend rodando na porta 3003!");
+});
+
+
